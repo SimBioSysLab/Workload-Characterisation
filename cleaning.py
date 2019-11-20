79ba1874cf2b50a03a4c23f53e4532045f0bff50
thebrahminator <vishwa.nath@outlook.com>
2019-11-20 17:49:24 -0500

    Added reading files

--- cleaning.py
+++ cleaning.py
@@ -1,2 +1,30 @@
+import csv
 import glob
 import pandas as pd
+
+HEADER_LIST = ["Timestamp", "Hostname", "DiskNumber", "Type", "Offset", "Size", "ResponseTime"]
+
+
+def convert_to_df():
+    pass
+
+
+def read_file(filename):
+    file_descriptor = open(filename, "r")
+    file_reader = csv.reader(file_descriptor, delimiter=",")
+    print(list(file_reader))
+    # for row in file_reader:
+    #     print(row)
+
+
+def get_file_names():
+    filenames = glob.glob("./dataset/*.csv")
+    read_file(filenames[0])
+
+
+def run():
+    get_file_names()
+
+
+if __name__ == '__main__':
+    run()

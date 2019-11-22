d720737d2b7cdcdaad8eebfed633685ff67a69a1
thebrahminator <vishwa.nath@outlook.com>
2019-11-21 19:20:02 -0500

    Added stuff

--- cleaning.py
+++ cleaning.py
@@ -1,20 +1,31 @@
 import csv
 import glob
+from datetime import datetime, timedelta, tzinfo
+from calendar import timegm
 import pandas as pd
 
+EPOCH_AS_FILETIME = 116444736000000000  # January 1, 1970 as MS file time
+HUNDREDS_OF_NANOSECONDS = 10000000
 HEADER_LIST = ["Timestamp", "Hostname", "DiskNumber", "Type", "Offset", "Size", "ResponseTime"]
 
 
-def convert_to_df():
-    pass
+def convert_filetime_to_dt(filetime):
+    return datetime.utcfromtimestamp((filetime - EPOCH_AS_FILETIME) / HUNDREDS_OF_NANOSECONDS)
+
+
+def convert_to_df(list_of_values):
+
+    for value in list_of_values:
+        value[0] = convert_filetime_to_dt(int(value[0]))
+        print(value)
+        exit()
 
 
 def read_file(filename):
     file_descriptor = open(filename, "r")
     file_reader = csv.reader(file_descriptor, delimiter=",")
-    print(list(file_reader))
-    # for row in file_reader:
-    #     print(row)
+    all_values = list(file_reader)
+    convert_to_df(all_values)
 
 
 def get_file_names():

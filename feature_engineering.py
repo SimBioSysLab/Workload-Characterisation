361028d89aba6aa0c1037e249df284a20c3e123d
thebrahminator <vishwa.nath@outlook.com>
2019-11-30 14:42:58 -0500

    Added data

--- feature_engineering.py
+++ feature_engineering.py
@@ -8,8 +8,10 @@ from utils import get_all_cleaned_files
 
 
 def derive_interarrival_times(file_name):
-    df_dataset = pd.read_csv(file_name)
-    print(df_dataset.head())
+    df_dataset = pd.read_csv(file_name, parse_dates=["Timestamp"])
+    df_dup = df_dataset["Timestamp"].copy(deep=True)
+    
+
     exit()
 
 
@@ -22,15 +24,13 @@ def convert_hostname_to_number(file_name):
     df_dataset.to_csv(file_name)
 
 
-def convert_functionality_to_number(file_name):
-    pass
-
-
 def run():
     logging.info(msg="Logging Feature Engineering.")
     cleaned_files_path = get_all_cleaned_files()
+    # for file_ in cleaned_files_path:
+    #     convert_hostname_to_number(file_name=file_)
     for file_ in cleaned_files_path:
-        convert_hostname_to_number(file_name=file_)
+        derive_interarrival_times(file_name=file_)
 
 
 if __name__ == "__main__":

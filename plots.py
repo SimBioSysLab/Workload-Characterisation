172d487ed9183b8a04aed19766202d148bcc940a
thebrahminator <vishwa.nath@outlook.com>
2019-12-03 13:19:48 -0500

    Added histogram plots
    Added plots to compute Histogram Values in Log Scale.
    Added Logging Features for Easier Computations.
    Removed Garbage Codebase.

--- plots.py
+++ plots.py
@@ -7,22 +7,17 @@ from utils import get_all_cleaned_files
 from loadconfig import config
 
 
-def interarrival_box_plot(in_ar_ti, plot_title):
-    title = "Box plot for {}".format(plot_title)
+def interarrival_hist_plot(in_ar_ti, plot_title):
+    title = "Histogram for {}".format(plot_title)
     plot_title = plot_title + "hist.png"
     graph_path = "./{}/{}".format(config.config_["GRAPH_FOLDER"], plot_title)
-    # box_plot = pygal.Box()
-    # box_plot.title = title
-    # box_plot.add(plot_title, in_ar_ti["InterarrivalTime"].tolist()[1:])
-    # box_plot.render()
 
     dataframe = pd.DataFrame(columns=["InterarrivalTime"])
     dataframe["InterarrivalTime"] = in_ar_ti["InterarrivalTime"]
-
-    dataframe.plot.hist(alpha=0.5, bins=15, grid=True, legend=None)
+    dataframe.dropna()
+    dataframe.plot.hist(alpha=0.5, bins=15, grid=True, legend=None, log=True)
     plt.xlabel("Interarrival Times")
     plt.title(title)
-    # plt.hist(in_ar_ti["InterarrivalTime"].tolist()[1:])
     plt.savefig(graph_path)
     logging.info("Saved file {}".format(graph_path))
 
@@ -34,10 +29,10 @@ def run():
         file_name = file_.split("/")[-1]
         file_name = file_name.split(".")[0]
         data_frame = pd.read_csv(file_)
-        print(max(data_frame["InterarrivalTime"].tolist()[1:]))
-        print(min(data_frame["InterarrivalTime"].tolist()[1:]))
-
-        interarrival_box_plot(in_ar_ti=data_frame, plot_title=file_name)
+        max_ = max(data_frame["InterarrivalTime"].tolist()[1:])
+        min_ = min(data_frame["InterarrivalTime"].tolist()[1:])
+        logging.info("For file {}, Max value is {}, Min value is {}".format(file_name, max_, min_))
+        interarrival_hist_plot(in_ar_ti=data_frame, plot_title=file_name)
 
 
 if __name__ == '__main__':

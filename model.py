62c87432c9e6a58f3b9e15566506542fc5d0cca1
thebrahminator <vishwa.nath@outlook.com>
2019-12-08 01:23:58 -0500

    Added data

--- model.py
+++ model.py
@@ -8,40 +8,34 @@ from scipy.cluster.hierarchy import dendrogram, linkage
 from sklearn.cluster import AgglomerativeClustering
 
 from loadconfig import config
-from utils import get_all_cleaned_files
+from utils import get_all_cleaned_files, get_project_dataset
 
 
 def building_model(dataset):
 
-    del dataset["Hostname"]
     del dataset["Timestamp"]
-    del dataset["Unnamed: 0"]
-    del dataset["DiskNumber"]
 
     dataset = dataset.dropna()
 
     print("Starting building the model")
-    clustering = AgglomerativeClustering(n_clusters=3, affinity="manhattan", linkage="complete")
+    clustering = AgglomerativeClustering(n_clusters=3)
 
-    clustering.fit(dataset.head(36000))
+    clustering.fit(dataset)
     print(clustering.labels_)
-    np.savetxt(X=clustering.labels_, fname="results.txt", fmt="%d")
+    np.savetxt(X=clustering.labels_, fname="results_3.txt", fmt="%d")
     pprint.pprint(clustering.labels_)
     logging.info(clustering.children_)
     logging.info(clustering.n_clusters_)
 
 
 def linkage_heirarchy(dataset):
-    del dataset["Hostname"]
     del dataset["Timestamp"]
-    del dataset["Unnamed: 0"]
-    del dataset["DiskNumber"]
 
     reduced_dataset = dataset.head(24000).dropna()
-    ip_dataset = linkage(reduced_dataset, "average")
-    dn = dendrogram(Z=ip_dataset, orientation="right", p=3, truncate_mode="level")
+    ip_dataset = linkage(reduced_dataset, "ward")
+    dn = dendrogram(Z=ip_dataset, orientation="right", truncate_mode="level")
     # print(dn)
-    plt.savefig("results.png")
+    plt.savefig("results_none.png")
     leaves_list = dn["leaves"]
     print(leaves_list)
     # print(ip_dataset)
@@ -49,9 +43,10 @@ def linkage_heirarchy(dataset):
 
 def run():
     files = get_all_cleaned_files()
-    dataset = pd.read_csv(files[0])
-    # building_model(dataset)
-    linkage_heirarchy(dataset)
+
+    dataset = pd.read_csv(get_project_dataset())
+    building_model(dataset)
+    # linkage_heirarchy(dataset)
 
 
 if __name__ == '__main__':

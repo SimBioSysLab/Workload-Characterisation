f9f6517914a095e23281050fede562a8cd018776
thebrahminator <vishwa.nath@outlook.com>
2019-12-07 19:15:18 -0500

    Added more code

--- model.py
+++ model.py
@@ -3,6 +3,8 @@ import pprint
 
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+from scipy.cluster.hierarchy import dendrogram, linkage
 from sklearn.cluster import AgglomerativeClustering
 
 from loadconfig import config
@@ -21,7 +23,7 @@ def building_model(dataset):
     print("Starting building the model")
     clustering = AgglomerativeClustering(n_clusters=3, affinity="manhattan", linkage="complete")
 
-    clustering.fit(dataset)
+    clustering.fit(dataset.head(36000))
     print(clustering.labels_)
     np.savetxt(X=clustering.labels_, fname="results.txt", fmt="%d")
     pprint.pprint(clustering.labels_)
@@ -29,10 +31,27 @@ def building_model(dataset):
     logging.info(clustering.n_clusters_)
 
 
+def linkage_heirarchy(dataset):
+    del dataset["Hostname"]
+    del dataset["Timestamp"]
+    del dataset["Unnamed: 0"]
+    del dataset["DiskNumber"]
+
+    reduced_dataset = dataset.head(24000).dropna()
+    ip_dataset = linkage(reduced_dataset, "average")
+    dn = dendrogram(Z=ip_dataset, orientation="right", p=3, truncate_mode="level")
+    # print(dn)
+    plt.savefig("results.png")
+    leaves_list = dn["leaves"]
+    print(leaves_list)
+    # print(ip_dataset)
+
+
 def run():
     files = get_all_cleaned_files()
     dataset = pd.read_csv(files[0])
-    building_model(dataset)
+    # building_model(dataset)
+    linkage_heirarchy(dataset)
 
 
 if __name__ == '__main__':

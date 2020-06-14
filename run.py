ff764db1965ec2a68de36d5d1096cee079b711b8
thebrahminator <vishwa.nath@outlook.com>
2020-06-14 19:07:03 -0400

    Added: Scatter plot for spatial locality
    Added feature to generate spatial locality graphs

--- run.py
+++ run.py
@@ -31,11 +31,10 @@ def run_cp_traces():
     st_time = time.time()
     # run_reading()
     # run_feature_engineering()
-    # run_generate_graphs()
+    run_generate_graphs()
     # run_combiner()
     # run_model()
     # run_hr_gen()
-    cleaning_run()
     end_time = time.time()
     time_ = end_time - st_time
     logging.info("Total running time is : {}".format(time_))

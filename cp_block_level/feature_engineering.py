f679f3428e7551856b77bbb81ea63a3774e7cc11
thebrahminator <vishwa.nath@outlook.com>
2020-08-22 10:46:33 -0400

    Added: Threading support for writing to files
    Added ability to thread multiple things together

--- cp_block_level/feature_engineering.py
+++ cp_block_level/feature_engineering.py
@@ -1,15 +1,13 @@
-import os
 import csv
 import json
 import logging
 import time
 import pandas as pd
-import arrow
 import threading
 
 from loadconfig import config
 from cp_block_level.utils import read_all_cpb_traces, extract_file_name, create_extraction_folders, \
-    get_extraction_folder
+    get_extraction_folder, create_multi_day_extraction, get_multi_extraction
 
 
 def read_write_count(file_path):
@@ -90,20 +88,110 @@ def thread_level_for_writing(obj, file_, day):
     logging.info("Finished writing to file: {}".format(file_name))
 
 
+def thread_split_writing_per_file(dataset_file, day):
+    print("Starting file: {} for day: {}".format(dataset_file, day))
+    data_f = open(dataset_file, "r")
+    dataset = csv.DictReader(data_f, fieldnames=config.config_["HEADERS"])
+    curr_time_final = None
+    second_file = None
+    second_writer = None
+    st_time = time.time()
+    if day == 1 or day == 7:
+        first_file = get_extraction_folder(file_name=dataset_file, day_num=day)
+        second_file = None
+    else:
+        first_file = get_extraction_folder(file_name=dataset_file, day_num=day)
+        base = 1
+        for i in range(2, day+1):
+            base = "{}{}".format(base, i)
+            second_file = get_multi_extraction(file_name=dataset_file, day_nums=base)
+
+    first_file = open(first_file, "w")
+    first_writer = csv.DictWriter(first_file, fieldnames=config.config_["HEADERS"])
+    first_writer.writeheader()
+
+    if second_file:
+        second_file = open(second_file, "w")
+        second_writer = csv.DictWriter(second_file, fieldnames=config.config_["HEADERS"])
+        second_writer.writeheader()
+
+    for row in dataset:
+        if not curr_time_final:
+            curr_time_final = int(row["ACCESS_TIME"])
+
+        time_diff = int(row["ACCESS_TIME"]) - curr_time_final
+        if day == 1:
+            if time_diff <= config.config_["DAY_MS"]:
+                first_writer.writerow(row)
+
+        if day == 2:
+            if config.config_["DAY_MS"] < time_diff <= 2 * config.config_["DAY_MS"]:
+                first_writer.writerow(row)
+            if time_diff <= 2 * config.config_["DAY_MS"]:
+                second_writer.writerow(row)
+
+        if day == 3:
+            if 2 * config.config_["DAY_MS"] < time_diff <= 3 * config.config_["DAY_MS"]:
+                first_writer.writerow(row)
+            if time_diff <= 3 * config.config_["DAY_MS"]:
+                second_writer.writerow(row)
+
+        if day == 4:
+            if 3 * config.config_["DAY_MS"] < time_diff <= 4 * config.config_["DAY_MS"]:
+                first_writer.writerow(row)
+            if time_diff <= 4 * config.config_["DAY_MS"]:
+                second_writer.writerow(row)
+
+        if day == 5:
+            if 4 * config.config_["DAY_MS"] < time_diff <= 5 * config.config_["DAY_MS"]:
+                first_writer.writerow(row)
+            if time_diff <= 5 * config.config_["DAY_MS"]:
+                second_writer.writerow(row)
+
+        if day == 6:
+            if 5 * config.config_["DAY_MS"] < time_diff <= 6 * config.config_["DAY_MS"]:
+                first_writer.writerow(row)
+            if time_diff <= 6 * config.config_["DAY_MS"]:
+                second_writer.writerow(row)
+
+        if day == 7:
+            if 6 * config.config_["DAY_MS"] < time_diff <= 7 * config.config_["DAY_MS"]:
+                first_writer.writerow(row)
+
+    end_time = time.time()
+    time_ = end_time - st_time
+    print("Finished running file {} for day {} in time {} seconds".format(dataset_file, day, time_))
+
+
 def split_feature_files(all_files_list):
     create_extraction_folders()
+    create_multi_day_extraction()
     for file_ in all_files_list:
-        st_time = time.time()
-        split_lists = split_files_into_days(dataset_file=file_)
-        for idx, day in enumerate(split_lists):
-            threads = threading.Thread(target=thread_level_for_writing, args=(day, file_, idx+1))
-            threads.start()
-        end_time = time.time()
-        time_ = end_time - st_time
-        logging.info("Finished splitting file: {} in {} seconds".format(file_, time_))
+        thread_list = []
+        for day in range(7):
+            t = threading.Thread(target=thread_split_writing_per_file, args=(file_, day+1))
+            thread_list.append(t)
+            t.start()
+
+        for val in thread_list:
+            val.join()
+
+    # for file_ in all_files_list:
+    #     st_time = time.time()
+    #     split_lists = split_files_into_days(dataset_file=file_)
+    #     for idx, day in enumerate(split_lists):
+    #         threads = threading.Thread(target=thread_level_for_writing, args=(day, file_, idx+1))
+    #         threads.start()
+    #     end_time = time.time()
+    #     time_ = end_time - st_time
+    #     logging.info("Finished splitting file: {} in {} seconds".format(file_, time_))
 
 
 def run_feature_engineering():
+    print("Input Path is: {}".format(config.config_["IP_PATH"]))
+    print("Output Path is: {}".format(config.config_["OP_PATH"]))
+    print("Aggregate Path is: {}".format(config.config_["OP_AGGR"]))
     all_trace_files = read_all_cpb_traces()
     # read_write_meta(all_files_list=all_trace_files)
-    split_feature_files(all_files_list=all_trace_files)
+    if config.config_["SPLIT_PATH"]:
+        split_feature_files(all_files_list=all_trace_files)

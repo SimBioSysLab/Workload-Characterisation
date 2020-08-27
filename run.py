f51ed739526bfebe0521cba8f66348bcee23f09d
thebrahminator <vishwa.nath@outlook.com>
2020-08-27 09:30:32 -0400

    Fix: For running the code from where it left off
    Fixed

--- run.py
+++ run.py
@@ -17,7 +17,7 @@ from msr_ml_proj.feature_engineering import run as feature_run
 from cp_block_level.feature_engineering import run_feature_engineering as cpb_feature_run
 
 
-def load_configuration(dataset, ip_path=None, op_path=None, op_aggr=None, split_file=False):
+def load_configuration(dataset, ip_path=None, op_path=None, op_aggr=None, split_file=False, compute_stat=False):
 
     assert dataset in ["cp", "msr", "cpb"], "Wrong Dataset Name"
 
@@ -27,15 +27,25 @@ def load_configuration(dataset, ip_path=None, op_path=None, op_aggr=None, split_
     if config.dataset == "cp":
         config.load_cp_yaml()
     if config.dataset == "cpb":
-        if not ip_path or not op_path:
-            print("Please enter current input and output path")
-            exit(0)
         if split_file:
-            if not os.path.isdir(op_path) or not os.path.exists(op_path) or not os.path.exists(op_aggr) or \
-                    not os.path.isdir(op_aggr):
-                print("Please enter path of folder to split files")
+            print(ip_path)
+            print(op_path)
+            print(op_aggr)
+            print(split_file)
+            print(compute_stat)
+            if not ip_path or not op_path or not os.path.isdir(op_path) or not os.path.exists(op_path) or not \
+                    os.path.exists(op_aggr) or not os.path.isdir(op_aggr):
+                print("Please enter the correct input and output path")
                 exit(0)
+
             config.load_cpb_yaml(ip_path=ip_path, op_path=op_path, op_aggr=op_aggr, split_file=split_file)
+        if compute_stat:
+            if not op_path or not os.path.isdir(op_path) or not os.path.exists(op_path) or not \
+                    os.path.exists(op_aggr) or not os.path.isdir(op_aggr):
+                print("Please enter the correct input and output path")
+                exit(0)
+            config.load_cpb_yaml(ip_path=ip_path, op_path=op_path, op_aggr=op_aggr, split_file=split_file,
+                                 compute_stat=True)
         else:
             pass
     config.load_logging_config()
@@ -80,17 +90,22 @@ if __name__ == '__main__':
     argument_parser.add_argument("--op_file", help="Output file path, only for cpb")
     argument_parser.add_argument("--op_aggr", help="Output file path for multiple days, cpb, if splitting")
     argument_parser.add_argument("--split_script", help="1 or 0 to split the files into particular slices")
+    argument_parser.add_argument("--compute_stat", help="1 or 0 to collect stats for day 1-7")
     args = argument_parser.parse_args()
     dataset_ = args.dataset
     ip_path = args.ip_file
     op_path = args.op_file
     op_aggr = args.op_aggr
     split_file = int(args.split_script)
+    compute_stat = int(args.compute_stat)
     if dataset_ == "cpb":
         if split_file == 1:
             load_configuration(dataset=dataset_, ip_path=ip_path, op_path=op_path, op_aggr=op_aggr, split_file=True)
+
+        if compute_stat == 1:
+            load_configuration(dataset=dataset_, op_path=op_path, op_aggr=op_aggr, split_file=False, compute_stat=True)
         else:
-            load_configuration(dataset=dataset_, ip_path=ip_path, op_path=op_path, split_file=False)
+            load_configuration(dataset=dataset_, op_path=op_path, op_aggr=op_aggr, split_file=False, compute_stat=False)
     else:
         load_configuration(dataset=dataset_)
 
